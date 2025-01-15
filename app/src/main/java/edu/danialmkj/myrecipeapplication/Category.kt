cc3ca48c21dd271b6fa648f3073616585c097f97
MohammaddMo <mohammadd.moazami007@gmail.com>
2025-01-15 12:14:49 +0330

    using Parselize Keywords to access Serialize and Deserialize our Object

--- app/src/main/java/edu/danialmkj/myrecipeapplication/Category.kt
+++ app/src/main/java/edu/danialmkj/myrecipeapplication/Category.kt
@@ -1,12 +1,17 @@
 package edu.danialmkj.myrecipeapplication
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 //create category data class
+//by adding @Parcelize (at first) & Parcelable(at the End of Object) keywords we can [[serialize]] and [[deserialize]] our {Object}
+@Parcelize
 data class Category(
     var idCategory: String,
     var strCategory: String,
     var strCategoryThumb: String,
     var strCategoryDescription: String
-)
+) : Parcelable
 
 
 //create list of category class
